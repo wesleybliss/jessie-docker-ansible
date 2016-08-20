3a24722037be646ab006f9576d3a35323dc05217
Wesley Bliss <wesley.bliss@gmail.com>
2016-08-19 23:51:05 -0400

    Added readme.

--- src/index.js
+++ src/index.js
@@ -1,12 +1,28 @@
 'use strict'
 
+const os = require('os')
 const express = require('express')
 const app = express()
 
-app.get('/', (req, res) => {
-    res.send('Hello World!');
+let ip
+let port = 3000
+let ifaces = os.networkInterfaces()
+
+for ( let dev in ifaces ) {
+    
+    let iface = ifaces[dev].filter( details => {
+        return details.family === 'IPv4' && details.internal === false
+    })
+    
+    if ( iface.length > 0 )
+        ip = iface[0].address
+    
+}
+
+app.get( '/', ( req, res ) => {
+    res.send( 'Hello I am app, nice to meet you.' );
 })
 
-app.listen(3000, () => {
-    console.log('Example app listening on port 3000!')
+app.listen( port, () => {
+    console.log( 'App listening on', ip, 'at', port )
 })
